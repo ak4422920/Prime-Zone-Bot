4e6b858ebc51564630fbf58f60b92263ff93d10f
ak4422920 <ak4422920@gmail.com>
2025-12-28 16:47:10 +0530

    Update cmds.py

--- TechifyBots/cmds.py
+++ TechifyBots/cmds.py
@@ -77,7 +77,7 @@ async def send_random_video(client: Client, message: Message):
                 chat_id=message.chat.id,
                 from_chat_id=DATABASE_CHANNEL_ID,
                 message_id=video_id,
-                caption=caption_text
+                caption=caption_text,
                     protect_content=True)
             await mdb.increment_daily_count(user_id)
             await asyncio.sleep(600)
@@ -85,6 +85,7 @@ async def send_random_video(client: Client, message: Message):
         except Exception as e:
             print(f"Error sending video: {e}")
             await message.reply_text("Failed to send video..")
+
 
 
 
